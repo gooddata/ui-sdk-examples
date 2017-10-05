0dd2cd5d1edfd9034c966dff18141bfb0ed3b4eb
Pavel Kolesnikov <pavel.kolesnikov@gmail.com>
2017-10-04 22:14:24 -0700

    Getting started demo app

---
+++ getting-started/README.md
@@ -0,0 +1,7 @@
+This simplistic app shows how to use GoodData UI SDK to display a result
+of a metric defined in a GoodData workspace.
+
+This app was created according to the user guide available from [here -
+TODO GH pages](https://todo).
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). You can find more information on how to perform common tasks with the create-react-app tooling [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
---
+++ getting-started/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Kpi } from '@gooddata/react-components';
+import '@gooddata/react-components/styles/css/main.css';
+import C from './catalog.json';
+ 
+import logo from './logo.svg';
+import './App.css';
+ 
+class App extends Component {
+   render() {
+      return (
+         <div className="App">
+            <div className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+               <h2>Welcome to React</h2>
+            </div>
+            <Kpi
+               projectId="la84vcyhrq8jwbu4wpipw66q2sqeb923"
+               measure={C['Avg Deal Size']} />
+            <p className="App-intro">
+               To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+         </div>
+      );
+   }
+}
+ 
+export default App;
