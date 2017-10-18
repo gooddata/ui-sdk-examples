ce49c21c2b59758134365ba54a3ed43df50ca946
Pavel Kolesnikov <pavel.kolesnikov@gmail.com>
2017-10-18 15:19:12 -0700

    Simplistic demo of interactive UI with a stateful component

---
+++ interactivity-stateful-simple/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Kpi } from '@gooddata/react-components';
+
+import '@gooddata/react-components/styles/css/main.css';
+import C from './catalog.json';
+
+const METRIC_1 = 'Avg Activities per Rep'
+const METRIC_2 = 'Avg Created Opps'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      metric: METRIC_1
+    };
+  }
+
+  setMetric(metric) {
+    console.log('setMetric, metric = ', metric)
+    this.setState({
+      metric
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <select onChange={(e) => this.setMetric(e.target.value)}>
+          <option key={METRIC_1}>{METRIC_1}</option>
+          <option key={METRIC_2}>{METRIC_2}</option>
+        </select>
+
+        <div>
+          {this.state.metric} is {' '}
+          <Kpi
+             projectId="la84vcyhrq8jwbu4wpipw66q2sqeb923"
+             measure={C[this.state.metric]}
+             format="#,##0" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
