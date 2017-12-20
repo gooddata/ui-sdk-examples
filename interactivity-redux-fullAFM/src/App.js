bb34df7cbd9702581138fc7f388026df42a94f23
Pavel Kolesnikov <pavel.kolesnikov@gmail.com>
2017-12-19 21:14:06 -0800

    More comments in Full AFM demo App.js

--- interactivity-redux-fullAFM/src/App.js
+++ interactivity-redux-fullAFM/src/App.js
@@ -12,6 +12,18 @@ import C from './catalog'
 
 const projectId = { projectId: "la84vcyhrq8jwbu4wpipw66q2sqeb923" }
 
+/*
+ * A simple presentation component that renders a dropdown
+ * populated with metrics passed via the "available" property
+ * and associates it with action creators to be received from
+ * a container component (the MeasureDropdown component defined
+ * right underneath).
+ *
+ * The action created by the setMeasures action creator will
+ * result into putting the specified measure(s) into a metric
+ * group so it can be used by the wrapper components imported
+ * from './components/smart'.
+ */
 const PureMeasureDropdown = ({ measures, available, setMeasures, measureGroup }) => {
   const selected = (measures && measures.length > 0) ? measures[0] : AVAILABLE_MEASURES[0]
   return (
@@ -28,7 +40,14 @@ const PureMeasureDropdown = ({ measures, available, setMeasures, measureGroup })
 
 const MeasureDropdown = afmConnect(PureMeasureDropdown)
 
-const PureAttributeElements = ({ attributeLabel, filterGroup, updateAttributeFilter }) => {
+/*
+ * A simple presentation component that renders a dropdown
+ * with attribute values for a given attributeLabel.
+ *
+ * The updateAttributeFilter is analogous to the setMeasures
+ * action creator described above.
+ */
+const PureAttributeElementsDropdown = ({ attributeLabel, filterGroup, updateAttributeFilter }) => {
   const afm = { attributes: [ { id: attributeLabel, type: "attribute" } ]}
   return (
     <Execute {...projectId} afm={afm} onLoadingChanged={e=>{}} onError={e=>{}}>{
@@ -47,18 +66,24 @@ const PureAttributeElements = ({ attributeLabel, filterGroup, updateAttributeFil
   )
 }
 
-const AttributeElements = afmConnect(PureAttributeElements)
+const AttributeElementsDropdown = afmConnect(PureAttributeElementsDropdown)
 
 const measureTransformation = AVAILABLE_MEASURES.map(measureName =>
   ({ id: C.metric(measureName), title: measureName })
 )
 
+/*
+ * A sample application component demonstrates how measureGroups
+ * and filterGroups are used to connect input controls (such as
+ * MeasureDropdown or AttributeElementsDropdown) with visualization
+ * components imported from './components/smart'.
+ */
 const App = () => (
   <div className="App">
     <span>Select measure:</span>
     <MeasureDropdown measureGroup="mg1" available={AVAILABLE_MEASURES} />
     <span> AccountRegion:</span>
-    <AttributeElements attributeLabel={C.attributeDisplayForm("Account Region")} filterGroup="fg1" />
+    <AttributeElementsDropdown attributeLabel={C.attributeDisplayForm("Account Region")} filterGroup="fg1" />
     <div>
       <Kpi measureGroup="mg1" filterGroup="fg1"
         measure={C.metric(AVAILABLE_MEASURES[0])}
