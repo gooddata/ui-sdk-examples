b66fb716303788025a0f201b6b5053f85810fb65
Tomáš Vojtášek <tomas.vojtasek@gooddata.com>
2018-01-19 08:56:08 +0100

    Fixed Typescript & React dependencies

--- angular-v4.x/src/app/gooddata-kpi.component.ts
+++ angular-v4.x/src/app/gooddata-kpi.component.ts
@@ -3,7 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as uuid from 'uuid';
 import * as invariant from 'invariant';
- 
+
 import { Component, Input, OnInit, OnDestroy, OnChanges, AfterViewInit } from '@angular/core';
 import { Kpi } from '@gooddata/react-components';
 
@@ -27,16 +27,15 @@ export class KpiComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit
     @Input() format: string;
     @Input() onLoadingChanged?: (any);
     @Input() onError?: (any);
- 
- 
+
     private rootDomID: string;
-     
+
     protected getRootDomNode() {
         const node = document.getElementById(this.rootDomID);
         invariant(node, `Node '${this.rootDomID} not found!`);
         return node;
     }
- 
+
     protected getProps(): KpiProps {
         const {
             projectId,
@@ -55,29 +54,29 @@ export class KpiComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit
             onError
         };
     }
- 
+
     private isMounted(): boolean {
         return !!this.rootDomID;
     }
- 
+
     protected render() {
         if (this.isMounted()) {
             ReactDOM.render(React.createElement(Kpi, this.getProps()), this.getRootDomNode());
         }
     }
- 
+
     ngOnInit() {
         this.rootDomID = uuid.v1();
     }
- 
+
     ngOnChanges() {
         this.render();
     }
- 
+
     ngAfterViewInit() {
         this.render();
     }
- 
+
     ngOnDestroy() {
         // Uncomment if Angular 4 issue that ngOnDestroy is called AFTER DOM node removal is resolved
         // ReactDOM.unmountComponentAtNode(this.getRootDomNode())
