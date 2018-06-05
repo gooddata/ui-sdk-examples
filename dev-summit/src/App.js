25b067f29f721bba0102ddb2768de19ce662efaf
Jiří Zajíc <jiri.zajic@mensa.cz>
2018-06-06 00:25:44 +0200

    dev-summit - <ColumnChart />

--- dev-summit/src/App.js
+++ dev-summit/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Kpi, Visualization } from '@gooddata/react-components';
+import { Kpi, Visualization, ColumnChart } from '@gooddata/react-components';
 import { CatalogHelper } from '@gooddata/react-components';
 import catalogJson from './catalog.json';
 import logo from './logo.svg';
@@ -25,6 +25,47 @@ class App extends Component {
             <Visualization
               projectId="xms7ga4tf3g3nzucd8380o2bev8oeknp"
               identifier="aby6oS6DbpFX"
+            />
+          </div>
+          <div style={{ height: 400 }}>
+            <ColumnChart
+              projectId="xms7ga4tf3g3nzucd8380o2bev8oeknp"
+              config={{
+                colors: ['#5B9BD5', '#FFC819', '#6B67DB', '#FFD962', '#FFBE62', '#82B7E6', '#908DE9', '#FFE388', '#FFCE88'],
+                legend: {
+                  enabled: false
+                }
+              }}
+              measures={[{
+                measure: {
+                  localIdentifier: 'm1',
+                  alias: '# Checks 💰',
+                  definition: {
+                    measureDefinition: {
+                      item: {
+                        identifier: C.measure('# Checks')
+                      }
+                    }
+                  }
+                }
+              }]}
+              viewBy={{
+                visualizationAttribute: {
+                  localIdentifier: 'a1',
+                  alias: 'Location 🏙',
+                  displayForm: {
+                    identifier: C.attributeDisplayForm('Location City')
+                  }
+                }
+              }}
+              stackBy={{
+                visualizationAttribute: {
+                  localIdentifier: 'a2',
+                  displayForm: {
+                    identifier: C.attributeDisplayForm('Location Name')
+                  }
+                }
+              }}
             />
           </div>
         </div>
