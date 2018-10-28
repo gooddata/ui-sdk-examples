47fea34d1379a24b6cd8c3e63c70e44017ab64f8
Jiří Zajíc <jiri.zajic@mensa.cz>
2018-10-28 11:02:50 -0700

    gd-ui-boilerplate - Update gdc-afm-connect

--- gd-ui-boilerplate/src/components/afmConnected.js
+++ gd-ui-boilerplate/src/components/afmConnected.js
@@ -15,31 +15,19 @@ import { afmConnect, afmWrapper } from '@gooddata/gdc-afm-connect';
 
 export const Kpi = afmConnect(KpiOrig);
 
-
 export const BarChart = afmConnect(BarChartOrig);
-
 export const ColumnChart = afmConnect(ColumnChartOrig);
-
 export const LineChart = afmConnect(LineChartOrig);
-
 export const PieChart = afmConnect(PieChartOrig);
-
 export const AreaChart = afmConnect(AreaChartOrig)
-
 export const Table = afmConnect(TableOrig);
-
 
 export const Execute = afmWrapper(ExecuteOrig);
 
-
+/* following AFM components are deprecated, kept only for backwards compability */
 export const AfmBarChart = afmWrapper(AfmComponents.BarChart);
-
 export const AfmColumnChart = afmWrapper(AfmComponents.ColumnChart);
-
 export const AfmLineChart = afmWrapper(AfmComponents.LineChart);
-
 export const AfmPieChart = afmWrapper(AfmComponents.PieChart);
-
 export const AfmAreaChart = afmWrapper(AfmComponents.AreaChart)
-
 export const AfmTable = afmWrapper(AfmComponents.Table);
