7fbca3cd82aea0659e9558a2c64597be9ddcbb27
Jiří Zajíc <jiri.zajic@mensa.cz>
2019-01-25 11:52:15 -0800

    gd-ui-boilerplate - iframe filter respects pixel-perfect filter

--- gd-ui-boilerplate/src/App.js
+++ gd-ui-boilerplate/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Kpi, ColumnChart, Execute, Visualization } from './components/afmConnected';
 import { Model } from '@gooddata/react-components';
+import { AfmActions } from '@gooddata/gdc-afm-connect';
+import { connect } from 'react-redux';
 import C from './catalog';
 import config from './config';
 import { FG_MAIN } from './constants';
@@ -11,8 +13,21 @@ import '@gooddata/react-components/styles/css/main.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleCityFilterChanged = this.handleCityFilterChanged.bind(this);
+  }
+
   componentDidMount() {
-    window.addEventListener('message', message => {
+    window.addEventListener('message', this.handleCityFilterChanged);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleCityFilterChanged);
+  }
+
+  handleCityFilterChanged(message) {
       // catching postMessages when embedded into pixel-perfect dashboard
       let data;
 
@@ -27,9 +42,12 @@ class App extends Component {
       }
 
       if (data && data.gdc && data.gdc.name === 'filter.value.changed') {
-        console.log(data.gdc.data);
+        const values = data.gdc.data;
+
+        if (values.length && values[0].label === C.attributeDisplayForm('Location City')) {
+          this.props.cityFilterChanged(values)
+        }
       }
-    });
   }
 
   render() {
@@ -94,4 +112,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  cityFilterChanged: (values) => dispatch(AfmActions.updatePositiveAttributeFilter(
+    FG_MAIN,
+    C.attributeDisplayForm('Location City'),
+    values.map(value => value.elementUri)
+  ))
+});
+
+export default connect(null, mapDispatchToProps)(App);
